7bdddff44f23ebc05d201624e2021ee3f1e46e7f
Yuvic1912121912 <yuvichanan27@gmail.com>
2026-04-16 22:13:18 +0000

    upgrade test assembly

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("This is a test of the .NET 10.0 runtime in WebAssembly.");
+        Console.WriteLine("The current date and time is: " + DateTime.Now);
+
+    }
+    [JSExport]
+    static string test()
+    {
+        Console.WriteLine("test was called from js");
+        Console.WriteLine("The current date and time is: " + DateTime.Now);
+        Console.WriteLine("type something into console");
+        string input = Console.ReadLine();
+        Console.WriteLine("you typed: " + input);
+        return input;
+    }
+}
--- test/obj/Debug/test.AssemblyInfo.cs
+++ test/obj/Debug/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c734ecdf9f774ed57d7a0084a63746eda3fbfcd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e6d883aecb5a7bf476645eb28b92503f188035")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
